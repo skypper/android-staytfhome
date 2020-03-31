25d0444dc9f3d0171f6c0ea4f35dbf6f30e3acd9
Tudor Anastasiu <tg.anastasiu@gmail.com>
2020-03-31 15:06:19 +0300

    Add BASE_URL constant to Heroku backend.

--- app/src/main/java/com/anastasiu/staytfhome/utils/Contants.kt
+++ app/src/main/java/com/anastasiu/staytfhome/utils/Contants.kt
@@ -3,4 +3,5 @@ package com.anastasiu.staytfhome.utils
 const val NETWORK_CONNECT_TIMEOUT_MS: Long = 5000
 const val NETWORK_READ_TIMEOUT_MS: Long = 5000
 const val BASE_URL = "http://10.0.2.2:9000/"
+///const val BASE_URL = "https://guarded-thicket-45565.herokuapp.com/"
 const val API_KEY = "specialKey"
